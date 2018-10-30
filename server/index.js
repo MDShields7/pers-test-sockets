24cb9e2bdf2758c5f80aab15177152940ebec0d7
Matt Shields <matthew.duncan.shields@gmail.com>
2018-10-30 09:40:28 -0700

    https://socket.io/get-started/chat/ finished chat example, functions

--- server/index.js
+++ server/index.js
@@ -1,8 +1,34 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res){
-  res.send('<h1>Hello world</h1>');
+  res.sendFile(__dirname + '/index.html');
+});
+        //Notify server of connect / disconnect
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+        //Send user message to server
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+  });
+});
+        //Send an event to everyone
+io.emit('some event', { for: 'everyone' });
+        //Broadcast to some not all
+io.on('connection', function(socket){
+  socket.broadcast.emit('hi');
+});
+        //Send the message to everyone
+io.on('connection', function(socket){
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
 });
 
 http.listen(3000, function(){
